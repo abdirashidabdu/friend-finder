ee7976506bd61e5206f9dcd65c0a122b91a930aa
Abdullahi <abdullahiabdirashid@gmail.com>
2018-08-06 22:40:34 -0500

    master

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var matches = [{
+  "name":"Ronaldo",
+  "photo":"https://www.gannett-cdn.com/-mm-/68cc3341f0f1e201482427038d9b54fc3c02f1b0/c=198-0-4475-3216/local/-/media/2018/08/05/USATODAY/USATODAY/636690805569848033-AP-Soccer-Juventus-Ronaldo.JPG?width=534&height=401&fit=crop",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Messi",
+  "photo":"http://dailypost.ng/wp-content/uploads/2017/01/lionel-messi-cropped_wvosiiy44-1.jpg",
+  "scores":[
+      1,
+      2,
+      4,
+      1,
+      2,
+      5,
+      4,
+      3,
+      3,
+      3
+    ]
+},
+{
+  "name":"Neymar Jr",
+  "photo":"http://weknowyourdreams.com/images/neymar/neymar-05.jpg",
+  "scores":[
+      1,
+      5,
+      1,
+      1,
+      2,
+      3,
+      4,
+      3,
+      1,
+      3
+    ]
+}]
+
+module.exports = matches;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,94 @@
+
+
+var matches = require("../data/friends");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(matches);
+  });
+
+  
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+          var userScores = req.body.scores;
+          var bestMatch = {};
+          var difference = 0;
+          var lowestDifference = 40;
+          var compareCurrent = [];
+          var parsedUserScores = [];
+
+          console.log(matches);
+          //console.log(userScores);
+          
+        for (i=0; i<userScores.length; i++) {
+            parsedUserScores.push(parseInt(userScores[i]));
+            console.log(parsedUserScores);
+        }
+
+
+      
+        for (i=0; i < matches.length; i++) {
+        
+        compareCurrent = matches[i].scores;
+        //compareCurrent.push(1);
+        console.log("current scores from data to compare: " + compareCurrent);
+        console.log("user submitted scores: " + parsedUserScores);
+        console.log("works!");
+        
+        for (x=0; x<compareCurrent.length; x++){
+          difference += Math.abs(compareCurrent[x] - parsedUserScores[x]);
+
+          console.log("Running difference: " + difference);
+          
+          };
+          
+          if(difference < lowestDifference) {
+              bestMatch = matches[i];
+              lowestDifference = difference;
+
+          }
+
+          
+        
+        console.log("Lowest Difference: " + lowestDifference);
+        console.log("Best Match: " + bestMatch); 
+        difference = 0;
+        };
+      
+
+        res.json(bestMatch);
+        
+      });
+     
+    
+      
+  };
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  
+
+
